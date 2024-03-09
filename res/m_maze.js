98acb758a86d855ecd81a44a891c5fd75fd1f8d3
Jens Hofschröer <github@nigjo.de>
2024-03-09 21:45:38 +0100

    Nummerische "Seeds" werden erkannt

--- res/m_maze.js
+++ res/m_maze.js
@@ -15,19 +15,22 @@ export default class Maze {
   constructor(width, height, seed = null) {
     if (typeof (seed) === 'string')
     {
-      //function hashCode(){
-      seed = ((s) => {
-        var hash = 0, i, chr;
-        if (s.length === 0)
+      if (seed.match(/^\d+$/)) {
+        seed = Number(seed);
+      } else {
+        //function hashCode(){
+        seed = ((s) => {
+          var hash = 0, i, chr;
+          if (s.length === 0)
+            return hash;
+          for (i = 0; i < s.length; i++) {
+            chr = s.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+          }
           return hash;
-        for (i = 0; i < s.length; i++) {
-          chr = s.charCodeAt(i);
-          hash = ((hash << 5) - hash) + chr;
-          hash |= 0; // Convert to 32bit integer
-        }
-        return hash;
-      })(seed);
-
+        })(seed);
+      }
     }
     seed = seed !== null ? seed : (width * height * 51);
     console.debug('seed', seed);
