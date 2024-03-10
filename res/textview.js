5f2e35d0be712b95ed9c8982139bcc955d709173
Jens Hofschröer <github@nigjo.de>
2024-03-10 10:11:57 +0100

    alternative Darstellung mit "Box Drawing" Rahmen möglich

--- res/textview.js
+++ res/textview.js
@@ -4,14 +4,23 @@ export default class TextView {
 
   constructor(maze) {
     this.maze = maze;
+    this.boxView = false;
   }
 
   create() {
+    if (this.boxView) {
+      return this.createBoxed();
+    } else {
+      return this.createAscii();
+    }
+  }
+
+  createAscii() {
     let m = this.maze;
     let pre = document.createElement('pre');
     let sep = '+' + '---+'.repeat(m.width) + '\n';
     let idx = m.entrance.col * 4 + 2;
-    pre.append(sep.substring(0, idx), '\u2193', sep.substring(idx + 1));
+    pre.append(sep.substring(0, idx), 'v', sep.substring(idx + 1));
     for (let r = 0; r < m.height; r++) {
       let cells = '|';
       let line = '+';
@@ -26,9 +35,49 @@ export default class TextView {
         if ((cell.walls & Maze.SOUTH) !== 0) {
           line += "---+";
         } else if (cell === m.exit) {
-          line += " \u2193 +";
+          line += "-v-+";
         } else {
           line += "   +";
+        }
+      }
+      pre.append(cells + "\n");
+      pre.append(line + "\n");
+    }
+    return pre;
+  }
+
+  createBoxed() {
+    let m = this.maze;
+    let pre = document.createElement('pre');
+    let sep = '\u2554'
+            + '\u2550\u2550\u2550\u2564'.repeat(m.width - 1)
+            + '\u2550\u2550\u2550\u2557\n';
+
+    let idx = m.entrance.col * 4 + 2;
+    pre.append(sep.substring(0, idx), '\u2193', sep.substring(idx + 1));
+    for (let r = 0; r < m.height; r++) {
+      let lastRow = r + 1 == m.height;
+      let cells = '\u2551';
+      let line = lastRow ? '\u255a' : '\u255f';
+      for (let c = 0; c < m.width; c++) {
+        let lastCol = c + 1 == m.width;
+        let cell = m.cells[r][c];
+        cells += " · ";
+        if ((cell.walls & Maze.EAST) !== 0) {
+          cells += lastCol ? "\u2551" : "\u2502";
+        } else {
+          cells += "·";
+        }
+        if ((cell.walls & Maze.SOUTH) !== 0) {
+          if (lastRow) {
+            line += "\u2550\u2550\u2550" + (lastCol ? "\u255d" : "\u2567");
+          } else {
+            line += "\u2500\u2500\u2500" + (lastCol ? "\u2562" : "\u253c");
+          }
+        } else if (cell === m.exit) {
+          line += "\u2550\u2193\u2550\u2567";
+        } else {
+          line += " · " + (lastCol ? "\u2562" : "\u253c");
         }
       }
       pre.append(cells + "\n");
