deb00ff4f28657ede6c41035107b1a108b22b5c6
Jens Hofschröer <git@nigjo.de>
2024-04-13 17:23:34 +0200

    Iso-Assets werden eingebunden

--- res/v_isometric.js
+++ res/v_isometric.js
@@ -1,4 +1,31 @@
 import SVGGenerator from './m_svgview.js';
+const svgDefs = await
+Promise.all([
+  fetch('res/isometric.svg').then(r => {
+    if (r.ok) {
+      return r.text();
+    }
+    throw r;
+  }).then(str => {
+    return new DOMParser().parseFromString(str, "image/svg+xml");
+  }),
+  fetch('res/isometric.css').then(r => {
+    if (r.ok) {
+      return r.text();
+    }
+    throw r;
+  })
+]).then((files) => {
+  let svg, css;
+  [svg, css] = files;
+  let defs = [...svg.querySelectorAll('defs>[id]')];
+  let styles = document.createElementNS(SVGGenerator.SVGNS, 'style');
+  styles.setAttribute('id','isostyles');
+  styles.textContent = css;
+  defs.push(styles);
+  //console.debug('defs', defs);
+  return new Map(defs.map(e => [e.id, e]));
+});
 
 export default class IsometricView extends SVGGenerator {
   constructor(maze) {
@@ -29,13 +56,20 @@ export default class IsometricView extends SVGGenerator {
   }
 
   initOutput(svg) {
+
+    let defs = document.createElementNS(IsometricView.SVGNS, 'defs');
+    for (let def of svgDefs.values()) {
+      defs.append(def.cloneNode(true));
+    }
+    svg.append(defs);
+
     //<link xmlns="http://www.w3.org/1999/xhtml" rel="stylesheet" href="isometric.css" type="text/css"/>
     let css = document.createElementNS("http://www.w3.org/1999/xhtml", 'link');
     css.setAttribute('rel', 'stylesheet');
     css.setAttribute('href', 'res/isometric.css');
     css.setAttribute('type', 'text/css');
     css.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml');
-    svg.append(css);
+    //svg.append(css);
 
     //<rect stroke="green" fill="#EEFFEE" x="0" y="0" width="480" height="480"/>
     let bg = document.createElementNS(IsometricView.SVGNS, 'rect');
@@ -50,7 +84,7 @@ export default class IsometricView extends SVGGenerator {
 
   tile(tile, cell) {
     if (tile === 'boden') {
-      return 'res/isometric.svg#boden';
+      return '#boden';
     } else if (tile.startsWith('door') || tile.startsWith('wall')) {
       if (tile.endsWith('Top') && cell.row > 0) {
         return;
@@ -58,7 +92,7 @@ export default class IsometricView extends SVGGenerator {
       if (tile.endsWith('Left') && cell.col > 0) {
         return;
       }
-      return 'res/isometric.svg#' + tile;
+      return '#' + tile;
     }
     return;
   }
