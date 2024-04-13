513637d196bf7694dc15e23433cd048c080e9cf8
Jens Hofschröer <git@nigjo.de>
2024-04-13 16:55:26 +0200

    Debugausgaben gelöscht

--- assets/vine.js
+++ assets/vine.js
@@ -3,23 +3,20 @@ export const asset = {
   tiles: {
     wallBottom: {
       transform: () => "translate(16,44)",
-      validator: (cell,id)=>{
-        console.debug('VINE', cell, id);
-        return id!=='vinesSingle';
+      validator: (_, id) => {
+        return id !== 'vinesSingle';
       }
     },
     doorLeft: {
       transform: () => "translate(4,40)",
-      validator: (cell,id)=>{
-        console.debug('VINE', cell, id);
-        return id==='vinesSingle';
+      validator: (_, id) => {
+        return id === 'vinesSingle';
       }
     },
     doorRight: {
       transform: () => "translate(48,40)",
-      validator: (cell,id)=>{
-        console.debug('VINE', cell, id);
-        return id==='vinesSingle';
+      validator: (_, id) => {
+        return id === 'vinesSingle';
       }
     }
   }
