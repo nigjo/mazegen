eee0887e72e13535e784c402bf1c66f0665f4542
Jens Hofschröer <git@nigjo.de>
2024-04-14 16:32:08 +0200

    eine erste Kiste

---
+++ assets/crate.js
@@ -0,0 +1,11 @@
+export const asset={
+  view:'crate.svg',
+  tiles:{
+    wallRight:{
+      transform:t=>"translate(30,20)"
+    },
+    wallLeft:{
+      transform:t=>"translate(12,24)"
+    }
+  }
+};
