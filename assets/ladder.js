af3814dd22fbde53305842e4f56ff01f3bfdebeb
Jens Hofschröer <git@nigjo.de>
2024-04-17 18:05:32 +0200

    Neues Leiter-Asset

---
+++ assets/ladder.js
@@ -0,0 +1,11 @@
+export const asset = {
+  view: 'ladder.svg',
+  tiles: {
+    doorRight: {
+      transform: t => "translate(47,39)"
+    },
+    doorLeft: {
+      transform: t => "translate(10.25,39)"
+    }
+  }
+};
