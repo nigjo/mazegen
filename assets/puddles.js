acefefed0ccc1076a1f4991f2ab0d74758662f1f
Jens Hofschröer <git@nigjo.de>
2024-05-01 22:07:47 +0200

    all puddles with z-value of 8

--- assets/puddles.js
+++ assets/puddles.js
@@ -2,22 +2,40 @@ export const asset = {
   view: 'puddles.svg',
   tiles: {
     boden: {
-      transform: () => 'translate(36,34)'
+      transform: (_, u) => {
+        u.setAttribute('data-z', 8);
+        return 'translate(36,34)';
+      }
     },
     decke: {
-      transform: () => 'translate(21,16)'
+      transform: (_, u) => {
+        u.setAttribute('data-z', 8);
+        return 'translate(21,16)';
+      }
     },
     doorLeft: {
-      transform: () => 'translate(8,31)'
+      transform: (_, u) => {
+        u.setAttribute('data-z', 8);
+        return 'translate(8,31)';
+      }
     },
     doorRight: {
-      transform: () => 'translate(52,18)'
+      transform: (_, u) => {
+        u.setAttribute('data-z', 8);
+        return 'translate(52,18)';
+      }
     },
     doorTop: {
-      transform: () => 'translate(35,0)'
+      transform: (_, u) => {
+        u.setAttribute('data-z', 8);
+        return 'translate(35,0)';
+      }
     },
     doorBottom: {
-      transform: () => 'translate(20,44)'
+      transform: (_, u) => {
+        u.setAttribute('data-z', 8);
+        return 'translate(20,44)';
+      }
     }
   }
 };
