461b7254c10b9761d8a9864ed067c2d28d262ea0
Jens Hofschröer <git@nigjo.de>
2024-05-01 22:04:58 +0200

    "stand" has new z value. reformatted file

--- assets/rescue.js
+++ assets/rescue.js
@@ -1,17 +1,20 @@
 export const asset = {
   view: 'rescue.svg',
   tiles: {
-    decke:{
-      transform:()=>"translate(34.5,9)",
-      validator:(_,t)=>t==='rescue-stand'
+    decke: {
+      transform: (_, u) => {
+        u.setAttribute('data-z', 16);
+        return "translate(34.5,9)";
+      },
+      validator: (_, t) => t === 'rescue-stand'
     },
-    wallBottom:{
-      transform:()=>"translate(37,33)",
-      validator:(_,t)=>t!=='rescue-stand'
+    wallBottom: {
+      transform: () => "translate(37,33)",
+      validator: (_, t) => t !== 'rescue-stand'
     },
-    wallLeft:{
-      transform:()=>"translate(19,29)",
-      validator:(_,t)=>t!=='rescue-stand'
+    wallLeft: {
+      transform: () => "translate(19,29)",
+      validator: (_, t) => t !== 'rescue-stand'
     }
-  }  
+  }
 };
